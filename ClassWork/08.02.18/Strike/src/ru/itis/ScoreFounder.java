052a71950fcdc7d60ee3fe16f978b4a18eb7e762
NitaKai <nito4kakay@gmail.com>
2018-02-08 23:36:38 +0300

    add refactor

--- ClassWork/08.02.18/Strike/src/ru/itis/ScoreFounder.java
+++ ClassWork/08.02.18/Strike/src/ru/itis/ScoreFounder.java
@@ -9,38 +9,38 @@ public class ScoreFounder {
     public int finalScore() throws FileNotFoundException {
 //		Считываем файл "Target"
         Scanner file = new Scanner(new File("Target"));
-		String next;
-		
+        String next;
+
 //		Обнуляем переменную, которая будет хранить информацию о сумме очков
         int score = 0;
-		
+
 //		Первое число в файле "Target" - количество кругов. Мы его считываем и сразу создаем массив кругов мишени
-		Ring [] rings = new Ring[Integer.parseInt(file.nextLine())];
+        Ring[] rings = new Ring[Integer.parseInt(file.nextLine())];
 //		заполняем информацию о радиусах. Она хранится так: первое число - радиус от центра
 //		второе - баллы за радиус
-        for (int count = 0; count < rings.length; count++){
+        for (int count = 0; count < rings.length; count++) {
 //			каждый раз сохраняем данные строки, для удобства использования
             next = file.nextLine();
 //			каждый раз создаем новый "круг"
             rings[count] = new Ring(Integer.parseInt(next.split(" ")[0]),
-                                    Integer.parseInt(next.split(" ")[1]));
+                    Integer.parseInt(next.split(" ")[1]));
         }
-		
+
 //		считываем выстрелы аналогично кругам. Разница лишь в том, что они хранятся через ", ", а не просто " "
-        Shoot [] shoots = new Shoot[Integer.parseInt(file.nextLine())];
-        for (int count = 0; count < shoots.length; count++){
+        Shoot[] shoots = new Shoot[Integer.parseInt(file.nextLine())];
+        for (int count = 0; count < shoots.length; count++) {
             next = file.nextLine();
             shoots[count] = new Shoot(Integer.parseInt(next.split(", ")[0]),
-                                        Integer.parseInt(next.split(", ")[1]));
+                    Integer.parseInt(next.split(", ")[1]));
         }
-		
+
 //		Проходимся по массиву выстрелов (выстрелов может быть больше чем мишеней и наоборот)
-        for (int count = 0; count < shoots.length; count++){
+        for (int count = 0; count < shoots.length; count++) {
 //			т.к. у нас баллы отсортированы по убыванию - можно не волноваться. Иначе пришлось бы сделать сортировку.
 //          Мы смотрим наименьшее число,
 //			расстояние от центра которого меньше, чем радиус
-            for (int inCount = 0; inCount < rings.length; inCount++){
-                if (shoots[count].getLength() < rings[inCount].getRadius()){
+            for (int inCount = 0; inCount < rings.length; inCount++) {
+                if (shoots[count].getLength() < rings[inCount].getRadius()) {
 //					Найдя такое - забираем баллы
                     score += rings[inCount].getScore();
 //					И ОБЯЗАТЕЛЬНО ОБРЫВАЕМ ЦИКЛ - ИНАЧЕ ОН ПОЙДЕТ ДАЛЬШЕ
@@ -49,7 +49,7 @@ public class ScoreFounder {
                 }
             }
         }
-		
+
 //		Ну и возращаем результат
         return score;
     }
--- ClassWork/08.02.18/Strike/src/ru/itis/Shoot.java
+++ ClassWork/08.02.18/Strike/src/ru/itis/Shoot.java
@@ -4,20 +4,21 @@ public class Shoot {
     private int x;
     private int y;
     private double length;
-//	класс хранит в себе только координаты
+
+    //	класс хранит в себе только координаты
     public Shoot(int x, int y) {
         this.x = x;
         this.y = y;
-		//высчитывается методом класса
+        //высчитывается методом класса
         length = lengthFounder(x, y);
     }
-	
-//	возвращает нам информацию о радиусе выстрела, смотря от центра
+
+    //	возвращает нам информацию о радиусе выстрела, смотря от центра
     public double getLength() {
         return length;
     }
-	
-//	ПИФАГОРОВЫ ШТАНЫ ВО ВСЕ СТОРОНЫ РАВНЫ
+
+    //	ПИФАГОРОВЫ ШТАНЫ ВО ВСЕ СТОРОНЫ РАВНЫ
     public double lengthFounder(int x, int y) {
         return (Math.sqrt(x * x + y * y));
     }
