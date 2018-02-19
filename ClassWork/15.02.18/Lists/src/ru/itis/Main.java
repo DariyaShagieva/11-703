16ffa824ce7c73a2b910f10d4a8078069bf8439f
NitaKai <nito4kakay@gmail.com>
2018-02-19 22:43:15 +0300

    add LinkedList

---
+++ ClassWork/15.02.18/Lists/src/ru/itis/Main.java
@@ -0,0 +1,31 @@
+package ru.itis;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    LinkedList list = new LinkedList();
+	    list.add(20);
+	    list.add("Ильяс");
+	    list.add("Влад 195");
+	    list.add("Андрей");
+        list.addToBegin("Nita");
+        list.add(45);
+        list.add(45);
+        list.add("Армейка");
+        list.remove("Андрей");
+        list.add("Bayrasheva");
+        list.add("I wanna go home");
+        list.remove("Nita");
+        list.remove("Армейка");
+        list.add("Андрей");
+        list.addToBegin("Marsel");
+        list.remove("I wanna go home");
+        list.add("Nita");
+        list.remove("Bayrasheva");
+//        System.out.println(list.contains("I wanna home"));
+        list.showList();
+        list.reverse();
+        list.showList();
+        int i = 0;
+    }
+}
