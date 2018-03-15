6df1fe2f0b76852365a8f1b80ad2d63b498f5be6
NitaKai <nito4kakay@gmail.com>
2018-03-15 20:14:00 +0300

    add structure to rp

--- HomeWork/SemesterWork/11/src/ru/itis/Main.java
+++
@@ -1,9 +0,0 @@
-package ru.itis;
-
-public class Main {
-
-    public static void main(String[] args) {
-        Dictionary dictionary = new Dictionary("huevoeNazvanie");
-        System.out.println(dictionary.numLen1());
-    }
-}
