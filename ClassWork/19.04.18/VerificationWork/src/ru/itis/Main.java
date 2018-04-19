702588be0f8f788711c51ec292849a707add81dd
NitaKai <nito4kakay@gmail.com>
2018-04-19 10:51:34 +0300

    add

--- ClassWork/19.04.18/VerificationWork/src/ru/itis/Main.java
+++ ClassWork/19.04.18/VerificationWork/src/ru/itis/Main.java
@@ -3,7 +3,9 @@ package ru.itis;
 public class Main {
 
     public static void main(String[] args) {
-        Scanner in = new Scanner();
+        Scanner in = new Scanner("input.txt");
+        System.out.println(in.nextInt());
+        System.out.println(in.nextInt());
         System.out.println(in.nextInt());
     }
 }
--- ClassWork/19.04.18/VerificationWork/src/ru/itis/Scanner.java
+++ ClassWork/19.04.18/VerificationWork/src/ru/itis/Scanner.java
@@ -1,7 +1,9 @@
 package ru.itis;
 
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 
 /**
@@ -12,20 +14,41 @@ import java.util.ArrayList;
  * @author Nita
  */
 public class Scanner {
+    int count;
+    FileInputStream inputStream;
+    int size;
+
+    public Scanner(String filePath) {
+        try {
+            size = (int) Files.size(Paths.get(filePath));
+            inputStream = new FileInputStream(filePath);
+            count = 0;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
     public int nextInt(){
-        InputStream currentChar = System.in;
-        int c;
-        StringBuilder builder = new StringBuilder();
+        int returnable = 0;
         try {
-            while ((c = currentChar.read()) != 10){
-                if (Character.isDigit((char)c) || (char)c == '-') {
-                    builder.append((char)c);
-                } else throw new IllegalStateException();
+            int c;
+            byte [] bytes = new byte[size];
+            int currentCount = count;
+            while ((c = inputStream.read()) != ' ' && count < size && c != -1){
+                bytes[count] = (byte) c;
+                count++;
             }
-        } catch (IOException e){
-            throw new IllegalStateException(e);
+            
+            int pow = 1;
+            for (int cur = count - 1; count - cur <= count - currentCount; cur--){
+                if (bytes[cur]!='-'){
+                    returnable += (bytes [cur] - '0') * pow;
+                    pow *= 10;
+                } else returnable*=-1;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-        return Integer.parseInt(String.valueOf(builder));
+        return returnable;
     }
 }
